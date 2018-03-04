bbce3c9f950856f05ab1903792eca200a78975a3
ekolpakov <ekolpakov@greet-go.com>
2018-03-04 21:51:26 +0600

    Added gradle autoconfiguration

---
+++ src/pompei/maths/two_masses/TwoMasses.java
@@ -0,0 +1,13 @@
+package pompei.maths.two_masses;
+
+public class TwoMasses {
+  public static void main(String[] args) {
+    TwoMasses twoMasses = new TwoMasses();
+    twoMasses.exec();
+
+  }
+
+  private void exec() {
+    System.out.println("HI");
+  }
+}
