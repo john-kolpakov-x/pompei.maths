6ff8decc57dc3c6f89a223002788d6b48f76987c
ekolpakov <ekolpakov@greet-go.com>
2018-03-24 11:53:40 +0600

    перед выведением классов из метода

--- test_src/pompei/maths/utils/collections/LinkedArray_optimizations_Test.java
+++ test_src/pompei/maths/utils/collections/LinkedArray_optimizations_Test.java
@@ -8,27 +8,96 @@ import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LinkedArray_optimizations_Test {
 
-  protected <E> LinkedArray<E> createArray() {
-    return LinkedArray.create();
+  interface Value {
+    String asStr();
+
+    default boolean isNone() {
+      return false;
+    }
   }
 
+  private static final Value NONE = new Value() {
+    @Override
+    public String asStr() {
+      return "None";
+    }
 
-  public static String oneTimeSec(long time, long count) {
+    @Override
+    public boolean isNone() {
+      return true;
+    }
+  };
+
+  public static Value oneTimeSec(long time, long count) {
+    if (count == 0) return NONE;
     double div = (double) time / (double) count / 1e9;
-    DecimalFormat fmt = new DecimalFormat("0.################");
+    DecimalFormat fmt = new DecimalFormat("0.0000000000000000");
     DecimalFormatSymbols sym = new DecimalFormatSymbols();
     sym.setGroupingSeparator(' ');
     sym.setDecimalSeparator('.');
     fmt.setDecimalFormatSymbols(sym);
-    return fmt.format(div);
+    return () -> fmt.format(div) + " sec";
   }
 
-  static class GettingStatistics {
+  static class Indicator implements Comparable<Indicator> {
+    public final String name;
+    public final Value value;
+
+    public Indicator(String name, Value value) {
+      Objects.requireNonNull(name);
+      Objects.requireNonNull(value);
+      this.name = name;
+      this.value = value;
+    }
+
+    public Indicator addPrefix(String prefix) {
+      return new Indicator(prefix == null ? name : prefix + name, value);
+    }
+
+    @Override
+    public String toString() {
+      return name + " = " + value.asStr();
+    }
+
+    public String name() {
+      return name == null ? "" : name;
+    }
+
+    @Override
+    public int compareTo(Indicator o) {
+      return name().compareTo(o.name());
+    }
+
+    public Indicator setNameLen(int len) {
+      StringBuilder sb = new StringBuilder();
+      sb.append(name());
+      while (sb.length() < len) sb.append(' ');
+      return new Indicator(sb.toString(), value);
+    }
+  }
+
+  abstract static class Statistics {
+    public abstract Stream<Indicator> indicators();
+
+    public Stream<Indicator> indicators(String prefix) {
+      return indicators().map(a -> a.addPrefix(prefix));
+    }
+
+    @Override
+    public String toString() {
+      return indicators().map(Indicator::toString).collect(Collectors.joining(", "));
+    }
+  }
+
+  static class GettingStatistics extends Statistics {
     long nullCount = 0, notNullCount = 0;
     long nullTime = 0, notNullTime = 0;
 
@@ -50,13 +119,22 @@ public class LinkedArray_optimizations_Test {
     }
 
     @Override
-    public String toString() {
-      return "call " + oneTimeSec(notNullTime, notNullCount) + " sec"
-        + ", null call " + oneTimeSec(nullTime, nullCount) + " sec";
+    public Stream<Indicator> indicators() {
+      return Stream.of(
+        new Indicator("call", oneTimeSec(notNullTime, notNullCount)),
+        new Indicator("not null", oneTimeSec(nullTime, nullCount))
+      );
+    }
+
+    public final void clean() {
+      nullCount = 0;
+      notNullCount = 0;
+      nullTime = 0;
+      notNullTime = 0;
     }
   }
 
-  static class PuttingStatistics {
+  static class PuttingStatistics extends Statistics {
     long putTime = 0, putCount = 0;
 
     public PuttingStatistics() {}
@@ -72,22 +150,33 @@ public class LinkedArray_optimizations_Test {
       return this;
     }
 
+    public final void clean() {
+      putTime = 0;
+      putCount = 0;
+    }
+
     @Override
-    public String toString() {
-      return "time " + oneTimeSec(putTime, putCount);
+    public Stream<Indicator> indicators() {
+      return Stream.of(new Indicator("put", oneTimeSec(putTime, putCount)));
     }
   }
 
   @Test
-  public void timing() throws Exception {
-    final LinkedArray<String> testing = createArray();
+  public void timing_original() throws Exception {
+    timing(LinkedArray.create());
+  }
+
+  private void timing(LinkedArray<String> original) throws Exception {
 
     final AtomicBoolean working = new AtomicBoolean(true);
     final AtomicBoolean showInfo = new AtomicBoolean(false);
 
+    abstract class MyThread extends Thread {
+      abstract void cleanStatistics();
+    }
 
-    class GettingThread extends Thread {
-      String get() {return null;}
+    abstract class GettingThread extends MyThread {
+      abstract String get();
 
       final GettingStatistics stat = new GettingStatistics();
       boolean localShowInfo = false;
@@ -98,9 +187,21 @@ public class LinkedArray_optimizations_Test {
         return toShow.get();
       }
 
+      final AtomicBoolean cleanStatistics = new AtomicBoolean(false);
+
+      @Override
+      void cleanStatistics() {
+        cleanStatistics.set(true);
+      }
+
       @Override
       public void run() {
         while (working.get()) {
+          if (cleanStatistics.get()) {
+            cleanStatistics.set(false);
+            stat.clean();
+            toShow.set(new GettingStatistics());
+          }
           String[] acceptor = new String[100];
           for (int i = 0, c = acceptor.length; i < c; i++) {
             long time = System.nanoTime();
@@ -140,18 +241,18 @@ public class LinkedArray_optimizations_Test {
     class LastGettingThread extends GettingThread {
       @Override
       String get() {
-        return testing.getAndRemoveLast();
+        return original.getAndRemoveLast();
       }
     }
 
     class FirstGettingThread extends GettingThread {
       @Override
       String get() {
-        return testing.getAndRemoveFirst();
+        return original.getAndRemoveFirst();
       }
     }
 
-    class PuttingThread extends Thread {
+    class PuttingThread extends MyThread {
       void put(String s) {}
 
       final List<String> array = new ArrayList<>();
@@ -170,11 +271,24 @@ public class LinkedArray_optimizations_Test {
         return toShow.get();
       }
 
+      final AtomicBoolean cleanStatistics = new AtomicBoolean(false);
+
+      @Override
+      void cleanStatistics() {
+        cleanStatistics.set(true);
+      }
+
       @Override
       public void run() {
         while (working.get()) {
 
           for (String s : array) {
+            if (cleanStatistics.get()) {
+              cleanStatistics.set(false);
+              stat.clean();
+              toShow.set(new PuttingStatistics());
+            }
+
             long time = System.nanoTime();
             put(s);
             stat.putTime += System.nanoTime() - time;
@@ -200,14 +314,14 @@ public class LinkedArray_optimizations_Test {
     class LastPuttingThread extends PuttingThread {
       @Override
       void put(String s) {
-        testing.putLast(s);
+        original.putLast(s);
       }
     }
 
     class FirstPuttingThread extends PuttingThread {
       @Override
       void put(String s) {
-        testing.putFirst(s);
+        original.putFirst(s);
       }
     }
 
@@ -231,6 +345,10 @@ public class LinkedArray_optimizations_Test {
     File workingFile = new File("build/LinkedArrayTest/timing.working");
     workingFile.getParentFile().mkdirs();
     workingFile.createNewFile();
+
+    File cleanStatisticsFile = new File("build/LinkedArrayTest/clean_statistics.do");
+    cleanStatisticsFile.getParentFile().mkdirs();
+    cleanStatisticsFile.createNewFile();
 
     while (workingFile.exists()) {
 
@@ -261,7 +379,17 @@ public class LinkedArray_optimizations_Test {
           .map(PuttingThread::showStat)
           .reduce(new PuttingStatistics(), PuttingStatistics::add);
 
-        showInfo("row", lastGetting, firstGetting, lastPutting, firstPutting);
+        showInfo("row ", lastGetting, firstGetting, lastPutting, firstPutting);
+      }
+
+      if (!cleanStatisticsFile.exists()) {
+        cleanStatisticsFile.createNewFile();
+
+        Stream.concat(
+          Stream.concat(lastGettingThreads.stream(), firstGettingThreads.stream()),
+          Stream.concat(lastPuttingThreads.stream(), firstPuttingThreads.stream())
+        ).parallel().forEach(MyThread::cleanStatistics);
+
       }
     }
 
@@ -301,9 +429,10 @@ public class LinkedArray_optimizations_Test {
         .map(PuttingThread::showStat)
         .reduce(new PuttingStatistics(), PuttingStatistics::add);
 
-      showInfo("TOTAL", lastGetting, firstGetting, lastPutting, firstPutting);
+      showInfo("TOTAL ", lastGetting, firstGetting, lastPutting, firstPutting);
     }
 
+    cleanStatisticsFile.delete();
   }
 
   private static void showInfo(String prefix,
@@ -312,12 +441,27 @@ public class LinkedArray_optimizations_Test {
                                PuttingStatistics lastPutting,
                                PuttingStatistics firstPutting) {
 
-    System.out.println("---------------------------------------------------------------------------------------------");
-    System.out.println(prefix + " lastGetting  " + lastGetting);
-    System.out.println(prefix + " firstGetting " + firstGetting);
-    System.out.println(prefix + " lastPutting  " + lastPutting);
-    System.out.println(prefix + " firstPutting " + firstPutting);
 
+    System.out.println("---------------------------------------------------------------------------------------------");
+
+    System.out.println(
+
+      Stream.concat(
+        lastGetting.indicators("getting last  "),
+        Stream.concat(
+          firstGetting.indicators("getting first "),
+          Stream.concat(
+            lastPutting.indicators("putting last  "),
+            firstPutting.indicators("putting first ")
+          )))
+        .map(a -> a.setNameLen(18))
+        .map(a -> a.addPrefix(prefix))
+        .map(a -> a.setNameLen(30))
+        .sorted()
+        .map(Indicator::toString)
+        .collect(Collectors.joining("\n"))
+
+    );
   }
 
   @Test
