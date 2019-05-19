fd52a32468a12b171d9914a01e3c64e739a03edb
ekolpakov <ekolpakov@greet-go.com>
2019-05-19 23:50:13 +0600

    Сделано перемещение

--- src/pompei/maths/lines_2d/core/MainContentPane.java
+++ src/pompei/maths/lines_2d/core/MainContentPane.java
@@ -8,7 +8,11 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseWheelEvent;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 public class MainContentPane extends JPanel {
 
@@ -27,6 +31,54 @@ public class MainContentPane extends JPanel {
           scaleChangeFactor = 1 / scaleChangeFactor;
         }
         viewPort.axes.changeScale(scaleChangeFactor, ViewVec2d.of(e.getPoint()));
+      }
+    });
+
+    final AtomicReference<ViewVec2d> mouseDownedAt = new AtomicReference<>(null);
+    final AtomicReference<ViewVec2d> offsetAtDowned = new AtomicReference<>(null);
+
+    Consumer<ViewVec2d> moveTo = mousePoint -> {
+      ViewVec2d mda = mouseDownedAt.get();
+      ViewVec2d startedOffset = offsetAtDowned.get();
+      if (mda == null || startedOffset == null) {
+        return;
+      }
+      var delta = mousePoint.minus(mda);
+      viewPort.axes.viewCenterOffset = startedOffset.plus(delta);
+    };
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      @Override
+      public void mouseDragged(MouseEvent e) {
+        moveTo.accept(ViewVec2d.of(e.getPoint()));
+      }
+    });
+
+    addMouseListener(new MouseAdapter() {
+      @Override
+      public void mousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+          mouseDownedAt.set(ViewVec2d.of(e.getPoint()));
+          offsetAtDowned.set(viewPort.axes.viewCenterOffset);
+        }
+        if (e.getButton() == MouseEvent.BUTTON2) {
+          mouseDownedAt.set(null);
+          var offset = offsetAtDowned.getAndSet(null);
+          if (offset != null) {
+            viewPort.axes.viewCenterOffset = offset;
+          }
+        }
+      }
+
+      @Override
+      public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+          ViewVec2d mda = mouseDownedAt.get();
+          if (mda != null) {
+            moveTo.accept(ViewVec2d.of(e.getPoint()));
+          }
+        }
+
       }
     });
 
