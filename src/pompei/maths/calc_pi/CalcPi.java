c852964932b975c1fca0f15fc83dbffbf28eaac3
ekolpakov <ekolpakov@greet-go.com>
2020-12-02 19:58:30 +0600

    Added CalcPi

---
+++ src/pompei/maths/calc_pi/CalcPi.java
@@ -0,0 +1,61 @@
+package probe.plugin;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class CalcPi {
+  final static MathContext mc = new MathContext(3000, RoundingMode.HALF_EVEN);
+  final static BigDecimal delta = new BigDecimal("0E-" + mc.getPrecision());
+
+  public static BigDecimal f(BigDecimal y) {
+    var y2 = y.multiply(y);
+    var y4 = y2.multiply(y2);
+    return BigDecimal.ONE.subtract(y4).sqrt(mc).sqrt(mc);
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println("delta = " + delta);
+
+    var _2 = BigDecimal.valueOf(2);
+    var _4 = BigDecimal.valueOf(4);
+
+    var a = BigDecimal.ONE;
+    var b = BigDecimal.ONE.divide(_2.sqrt(mc), mc);
+    var t = new BigDecimal("0.25");
+    var p = BigDecimal.ONE;
+
+    int count = 0;
+
+    while (true) {
+
+      var a1 = a.add(b).divide(_2, mc);
+
+      var b1 = a.multiply(b).sqrt(mc);
+
+      var deltaA = a1.subtract(a);
+
+      var t1 = t.subtract(p.multiply(deltaA.multiply(deltaA)));
+
+      var p1 = p.multiply(_2);
+
+      a = a1;
+      b = b1;
+      t = t1;
+      p = p1;
+
+      count++;
+
+      if ((a.subtract(b).abs().compareTo(deltaA)) <= 0) {
+        break;
+      }
+    }
+
+    var ab = a.add(b);
+    var pi = ab.multiply(ab).divide(_4.multiply(t), mc);
+    System.out.println("pi = " + pi);
+    System.out.println("count = [ " + count + " ]");
+
+  }
+}
